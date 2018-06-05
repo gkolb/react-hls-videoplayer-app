0a012be3beb0e7830579159b0292c1647bd2be90
gkolb <gkolb100@gmail.com>
2018-06-05 15:54:55 -0600

    First commit

---
+++ src/components/VideoPlayer/VideoPlayer.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class VideoPlayer extends Component {
+  render() {
+    return (
+      <div>
+        <video height="600" id="video"></video>
+      </div>
+    )
+  }
+}
+
+export default VideoPlayer;
